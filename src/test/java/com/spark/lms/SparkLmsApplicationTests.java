4c7cf57d6d46d3ebd5da4257a089dd30ecc07eb3
Md. Samin Irtiza <samin.irtiza@bjitacademy.com>
2023-07-11 16:18:29 +0600

    Disabled Test

--- src/test/java/com/spark/lms/SparkLmsApplicationTests.java
+++ src/test/java/com/spark/lms/SparkLmsApplicationTests.java
@@ -1,10 +1,10 @@
 package com.spark.lms;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Disabled
